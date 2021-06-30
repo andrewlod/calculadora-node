23d43e79a3a38a1bebe8b486492f0ce91a6e1111
andrewlod4kst <andre@4kst.com>
2021-06-30 15:29:03 -0300

    secrets

--- src/apps/MainApp.js
+++ src/apps/MainApp.js
@@ -2,6 +2,42 @@ var express = require("express");
 const {RouteCalculadora, RoutePing} = require("../routes");
 var bodyParser = require("body-parser");
 
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://aws.amazon.com/developers/getting-started/nodejs/
+
+// Load the AWS SDK
+var AWS = require('aws-sdk'),
+    region = "us-east-1",
+    secretName = "calculadora-node",
+    secret,
+    decodedBinarySecret;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    region: region
+});
+
+// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+// We rethrow the exception by default.
+
+client.getSecretValue({SecretId: secretName}, function(err, data) {
+    if (err) {
+        throw err;
+    }
+    else {
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = data.SecretString;
+        } else {
+            let buff = new Buffer(data.SecretBinary, 'base64');
+            decodedBinarySecret = buff.toString('ascii');
+        }
+    }
+});
+console.log(secrets)
 var app = express();
 
 app.use(bodyParser.json());
